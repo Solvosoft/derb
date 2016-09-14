d6d22c01a86a0aa117d32b22e89378c415c2ff76
Adolfo Hernández <fofoadolfo14@gmail.com>
2016-09-14 16:00:29 -0600

    Init Class

--- report_builder/Question/QuestionType/unique_selection_question.py
+++ report_builder/Question/QuestionType/unique_selection_question.py
@@ -4,3 +4,29 @@ Created on 14/9/2016
 @author: adolfo
 '''
 
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.utils import timezone
+
+from report_builder.Question.QuestionView import QuestionViewAdmin, QuestionViewResp, QuestionViewPDF
+
+
+class miContexto(object):
+
+    def get_context_data(self, **kwargs):
+        contex = ListView.get_context_data(self, **kwargs)
+        contex['datetime'] = timezone.now()
+        return contex
+
+class UniqueSelectionAdmin(QuestionViewAdmin):
+    def hola(self):
+        return print("hola")
+       
+class UniqueSelectionResp(QuestionViewResp):
+    def hola(self):
+        return print("hola")
+        
+class UniqueSelectionPDF(QuestionViewPDF):
+    def hola(self):
+        return print("hola")
--- report_builder/urls.py
+++ report_builder/urls.py
@@ -3,9 +3,22 @@ from django.conf.urls import url
 from report_builder import views
 
 from report_builder import initial_views as init
+from report_builder.Question.QuestionType.unique_selection_question import UniqueSelectionAdmin,\
+    UniqueSelectionResp, UniqueSelectionPDF
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^init$', init.InitialIndexView.as_view(), name='init'),
     url(r'^report/new/$', init.NewReportView.as_view(), name='new_report_template')
+]
+
+
+# Unique_Selection_Question
+urlpatterns += [
+    url(r"^unique_selection_admin$", UniqueSelectionAdmin.as_view(),
+        name="unique_selection_admin"),
+    url(r"^unique_selection_resp$", UniqueSelectionResp.as_view(),
+        name="unique_selection_resp"),
+    url(r"^unique_selection_pdf$", UniqueSelectionPDF.as_view(),
+        name="unique_selection_pdf"),
 ]
