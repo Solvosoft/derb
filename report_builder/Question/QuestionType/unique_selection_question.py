edbbb4364324288a2e580a81a098b92ef3b1de2e
Adolfo Hernández <fofoadolfo14@gmail.com>
2016-09-20 11:33:30 -0600

    Edit Views

--- report_builder/Question/QuestionType/unique_selection_question.py
+++ report_builder/Question/QuestionType/unique_selection_question.py
@@ -6,12 +6,13 @@ Created on 14/9/2016
 
 from django.shortcuts import render
 from report_builder.Question import QuestionView
+from report_builder.Question.forms import QuestionForm, AnswerForm
 
-class UniqueSelectionAdmin(QuestionView.QuestionViewAdmin):
+class UniqueSelectionAdmin(QuestionView.QuestionViewAdmin, QuestionForm):
     def get(self, request, *args, **kwargs):
         return render(request, 'admin/unique_selection_question.html')
        
-class UniqueSelectionResp(QuestionView.QuestionViewResp):
+class UniqueSelectionResp(QuestionView.QuestionViewResp, AnswerForm):
     def get(self, request, *args, **kwargs):
         return render(request, 'responsable/unique_selection_question.html')
         
