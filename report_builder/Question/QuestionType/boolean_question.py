eecf7a78e94ca457889bda7653eaffff293d6287
Mau Rodríguez <maucrodriguez@gmail.com>
2016-09-23 17:59:53 -0600

    Added boolean question views

---
+++ report_builder/Question/QuestionType/boolean_question.py
@@ -0,0 +1,15 @@
+from report_builder.Question.QuestionView import QuestionViewAdmin
+from report_builder.Question.QuestionView import QuestionViewResp
+from report_builder.Question.QuestionView import QuestionViewPDF
+
+
+class BooleanQuestionViewAdmin(QuestionViewAdmin):
+    pass
+
+
+class BooleanQuestionViewResp(QuestionViewResp):
+    pass
+
+
+class BooleanQuestionViewPDF(QuestionViewPDF):
+    pass
