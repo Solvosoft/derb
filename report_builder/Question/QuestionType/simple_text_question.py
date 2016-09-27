88518c9d178522ec8dd71bc7789b35fed9e5b023
Nashyra Lorig <nashyralb@gmail.com>
2016-09-27 17:03:37 -0600

    fix the simple text question view

--- report_builder/Question/QuestionType/simple_text_question.py
+++ report_builder/Question/QuestionType/simple_text_question.py
@@ -3,40 +3,56 @@ Created on 15/9/2016
 
 @author: nashyra
 '''
-from report_builder.Question.QuestionView import QuestionViewAdmin, QuestionViewResp, QuestionViewPDF
+import random
+
+
+from django.contrib import messages
 from django.shortcuts import render
-from report_builder.Question.forms import SimpleTextQuestionForm, SimpleTextResp
-from django.http import HttpResponse
+
+from report_builder.Question.QuestionView import QuestionViewAdmin
+from report_builder.Question.forms import SimpleTextQuestionForm
+from report_builder.models import Question, Answer, Report
 
 
 class SimpleTextQuestionAdmin(QuestionViewAdmin):
     form_class = SimpleTextQuestionForm
+    template_name = 'admin/simple_text_question.html'
+    name = 'simple_text_question'
+    minimal_representation = {
+        'human_readable_name': 'Simple text question',
+        'help': 'Allows you to make simple text questions',
+        'color': '#330065'
+    }
 
     def get(self, request, *args, **kwargs):
-        form = self.get_form()
+        self.form_number = random.randint(self.start_number, self.end_number)
+        self.request = request
+        form = self.get_form(instance=self.question)
         parameters = {
             'form': form,
+            'question': self.question,
+            'name': self.name,
+            'form_number': str(self.form_number),
+            'minimal_representation': self.minimal_representation
         }
-        return render(request, 'admin/simple_text_question.html', parameters)
-    
+        return render(request, self.template_name, parameters)
+
     def post(self, request, *args, **kwargs):
+        self.request = request
+        self.form_number = random.randint(self.start_number, self.end_number)
         form = self.get_form(request.POST, instance=self.question)
         if form.is_valid():
             question = form.save(False)
-            question.report = self.report
             question.class_to_load = self.name
-            question = self.pre_save(question, request, form)
+            question.report = Report.objects.first()
             question.save()
-
-        return HttpResponse(str(question.pk))
+            messages.add_message(request, messages.SUCCESS, 'Question created successfully')
+        else:
+            messages.add_message(request, messages.ERROR, 'An error ocurred while creating the question')
+        return self.get(request, *args, **kwargs)
     
 
-class SimpleTextQuestionResp(QuestionViewResp):
-    form_class = SimpleTextResp
-    def get(self, request, *args, **kwargs):
-        return render(request, 'responsable/simple_text_question.html')
+#class SimpleTextQuestionResp(QuestionViewResp):
 
 
-class SimpleTextQuestionPDF(QuestionViewPDF):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'pdf/simple_text_question.html')
+#class SimpleTextQuestionPDF(QuestionViewPDF):
