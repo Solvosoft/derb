07e91e54ed368b31dfbc84235344d0e1de52fc5c
Nashyra Lorig <nashyralb@gmail.com>
2016-09-29 16:48:56 -0600

    fixing the view for pdf

--- report_builder/Question/QuestionType/simple_text_question.py
+++ report_builder/Question/QuestionType/simple_text_question.py
@@ -6,13 +6,18 @@ Created on 15/9/2016
 import json
 import random
 
-
 from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import Context
+from django.template.loader import get_template
+from django.utils import timezone
+from weasyprint import HTML
 
-from report_builder.Question.QuestionView import QuestionViewAdmin, QuestionViewResp
+from report_builder.Question.QuestionView import QuestionViewAdmin, QuestionViewResp, QuestionViewPDF
 from report_builder.Question.forms import SimpleTextQuestionForm, SimpleTextAnswerForm
 from report_builder.models import Question, Answer, Report, ReportByProject
+
 
 
 class SimpleTextQuestionAdmin(QuestionViewAdmin):
@@ -110,5 +115,34 @@ class SimpleQuestionViewResp(QuestionViewResp):
         return self.get(request, *args, **kwargs)
 
 
-
 #class SimpleTextQuestionPDF(QuestionViewPDF):
+class SimpleTextQuestionPDF(QuestionViewPDF):
+    name = 'simple_text_question'
+    template_name = 'pdf/simple_text_question.html'
+
+    def get(self, request, *args, **kwargs):
+        self.request = request
+        self.question = Question.objects.get(pk=kwargs['question_pk'])
+        self.answer = Answer.objects.filter(question=self.question).first()
+
+        parameters = {
+            'name': self.name,
+            'question': self.question,
+            'question_number': self.question.order,
+            'answer': self.answer,
+            'form_number': str(random.randint(self.start_number, self.end_number)),
+            'datetime': timezone.now(),
+        }
+
+        template = get_template(self.template_name)
+
+        html = template.render(Context(parameters)).encode('UTF-8')
+
+        page = HTML(string=html, encoding='utf-8').write_pdf()
+
+        response = HttpResponse(page, content_type='application/pdf')
+
+        response[
+            'Content-Disposition'] = 'attachment; filename="question_report.pdf"'
+
+        return response
--- report_builder/urls.py
+++ report_builder/urls.py
@@ -17,5 +17,7 @@ urlpatterns += [
     url(r'^question_type/simple/admin', simple_text_question.SimpleTextQuestionAdmin.as_view(),
         name='simple_text_question_admin'),
     url(r'^question_type/simple/resp/(?P<question_pk>\d+)', simple_text_question.SimpleQuestionViewResp.as_view(),
-        name='simple_text_question_resp')
+        name='simple_text_question_resp'),
+    url(r'^question_type/simple/pdf/(?P<question_pk>\d+)', simple_text_question.SimpleTextQuestionPDF.as_view(),
+        name='simple_text_question_pdf')
 ]
