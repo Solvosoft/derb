abc6d3b3c3976fb19ff18d0074002a122251e14c
Natalia Parra <natyparra04@gmail.com>
2016-09-30 08:31:58 -0600

    create pdf view

--- report_builder/Question/QuestionType/integer_question.py
+++ report_builder/Question/QuestionType/integer_question.py
@@ -11,6 +11,12 @@ from report_builder.models import Report, Question, Answer, ReportByProject
 from report_builder.shortcuts import get_children
 import random
 from django.contrib import messages
+from _datetime import timezone
+from django.template.loader import get_template
+
+from django.http import HttpResponse
+from django.template import Context
+from weasyprint import HTML
 
 
 class IntegerQuestionAdmin(QuestionViewAdmin):
@@ -99,9 +105,10 @@ class IntegerQuestionResp(QuestionViewResp):
         minimum = answer_options['minimum']
         maximum = answer_options['maximum']
         steps = answer_options['steps']
-        print(minimum)
-    
+       # print(minimum)
+        answer= self.request.GET.get("integer_answer");
         parameters = {
+            'integer_answer':answer,
             'minimum':minimum,
             'maximum':maximum,
             'steps':steps,
@@ -112,6 +119,8 @@ class IntegerQuestionResp(QuestionViewResp):
             'answer': self.answer,
             'form_number': str(self.form_number)
         }
+        print('todo bien en get')
+        
         return render(request, self.template_name, parameters)
 
     def post(self, request, *args, **kwargs):
@@ -135,6 +144,10 @@ class IntegerQuestionResp(QuestionViewResp):
             answer.question = self.question
             answer.user = request.user
             answer.report = ReportByProject.objects.first()
+            integer_answer = self.request.POST.get("integer_answer")
+            answer.text= str(integer_answer)
+            answer.annotation=str(integer_answer)
+            answer.display_text=str(integer_answer)
             self.answer = answer
             self.save(answer)
             messages.add_message(request, messages.SUCCESS, 'Question answered successfully')
@@ -142,3 +155,34 @@ class IntegerQuestionResp(QuestionViewResp):
             messages.add_message(request, messages.ERROR, 'An error ocurred while answering the question')
 
         return self.get(request, *args, **kwargs)
+    
+class IntegerQuestionViewPDF(QuestionViewPDF):
+    name = 'integer_question'
+    template_name = 'pdf/integer_question.html'
+
+    def get(self, request, *args, **kwargs):
+        self.request = request
+        self.question = Question.objects.get(pk=kwargs['question_pk'])
+        self.answer = Answer.objects.filter(question=self.question).first()
+
+        parameters = {
+            'name': self.name,
+            'question': self.question,
+            'question_number': self.question.order,
+            'answer': self.answer,
+            'form_number': str(random.randint(self.start_number, self.end_number)),
+            #'datetime': timezone.now(),
+        }
+
+        template = get_template(self.template_name)
+
+        html = template.render(Context(parameters)).encode('UTF-8')
+
+        page = HTML(string=html, encoding='utf-8').write_pdf()
+
+        response = HttpResponse(page, content_type='application/pdf')
+
+        response[
+            'Content-Disposition'] = 'attachment; filename="question_report.pdf"'
+
+        return response
