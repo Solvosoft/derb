db326256d9b83a0bd68738536efd6afaa03695e1
Mau Rodríguez Morales <maucrodriguez@gmail.com>
2016-10-13 15:43:37 -0600

    Reestructure the JS code in the unique selection question admin view

--- presentation/static/report_builder/js/derb_subcomponents/question_types/unique_selection_question.js
+++ presentation/static/report_builder/js/derb_subcomponents/question_types/unique_selection_question.js
@@ -2,7 +2,17 @@
  * Created by jaquer on 10/10/16.
  */
 $(document).ready(function () {
-    $("#id_catalog option:selected").each(function () {
+    update_combo();
+
+    $("#id_catalog").change(function () {
+        update_combo();
+    });
+
+    initial_values();
+});
+
+function update_combo() {
+    $("#id_catalog").find("option:selected").each(function () {
         $('#catalog_name').html($(this).text());
         var catalog_id = $(this).val();
         $.ajax({
@@ -12,7 +22,8 @@ $(document).ready(function () {
                 catalog_id: catalog_id
             },
             success: function (data) {
-                $('#display_fields').html('');
+                var df_selector = $('#display_fields');
+                df_selector.html('');
 
                 var display_fields = data.content;
                 var html = '';
@@ -23,36 +34,14 @@ $(document).ready(function () {
                     html += '<ul><li><label for="' + id + '"><input id="' + id + '" type="checkbox" value="' + display_fields[i][0] + '" name="display_fields"> ' + display_fields[i][1] + '</label></li></ul>';
                     count++;
                 }
-                $('#display_fields').html(html);
+                df_selector.html(html);
             }
         });
     });
+}
 
-    $("#id_catalog").change(function () {
-        $("#id_catalog option:selected").each(function () {
-            $('#catalog_name').html($(this).text());
-            var catalog_id = $(this).val();
-            $.ajax({
-                url: catalog_url,
-                type: 'GET',
-                data: {
-                    catalog_id: catalog_id
-                },
-                success: function (data) {
-                    $('#display_fields').html('');
+function initial_values() {
+    var answer_options = JSON.parse(answer_options_json);
 
-                    var display_fields = data.content;
-                    var html = '';
-                    var count = 0;
-                    var id = '';
-                    for (var i = 0; i < display_fields.length; i++) {
-                        id = 'id_display_fields_' + count;
-                        html += '<ul><li><label for="' + id + '"><input id="' + id + '" type="checkbox" value="' + display_fields[i][0] + '" name="display_fields"> ' + display_fields[i][1] + '</label></li></ul>';
-                        count++;
-                    }
-                    $('#display_fields').html(html);
-                }
-            });
-        });
-    });
-});
+    /* TODO */
+}
