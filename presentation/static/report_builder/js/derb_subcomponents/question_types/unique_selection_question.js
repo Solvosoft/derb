a49d67f23e3c24bc2b3139f412267869eb9a7615
Mau Rodríguez <mauricio.rodriguez_mora@ucr.ac.cr>
2016-10-19 09:44:53 -0600

    Merged unique_selection_question into development

--- presentation/static/report_builder/js/derb_subcomponents/question_types/unique_selection_question.js
+++ presentation/static/report_builder/js/derb_subcomponents/question_types/unique_selection_question.js
@@ -2,6 +2,8 @@
  * Created by jaquer on 10/10/16.
  */
 $(document).ready(function () {
+    update_combo();
+
     $("#id_catalog").change(function () {
         update_combo();
     });
@@ -28,7 +30,7 @@ function update_combo() {
                 var count = 0;
                 var id = '';
                 for (var i = 0; i < display_fields.length; i++) {
-                    id = 'id_display_fields_' + count;
+                    id = 'id_display_fields_' + display_fields[i][1];
                     html += '<ul><li><label for="' + id + '"><input id="' + id + '" type="checkbox" value="' + display_fields[i][0] + '" name="display_fields"> ' + display_fields[i][1] + '</label></li></ul>';
                     count++;
                 }
@@ -44,16 +46,16 @@ function initial_values() {
             var answer_options = JSON.parse(answer_options_json);
             var catalog = answer_options.catalog;
             var display_fields = answer_options.display_fields;
-            var checkbox = $('[id^=id_display_fields_]');
 
             $('#id_catalog').val(catalog);
             update_combo();
 
-            for (var i = 0; i < checkbox.length; i++) {
-                if (display_fields.indexOf($(checkbox[i]).attr('value')) != -1) {
-                    $(checkbox[i]).prop('checked', true);
-                }
-            }
+            window.setTimeout(function() {
+            	for (var i=0; i < display_fields.length; i++){
+            		$('#id_display_fields_'+display_fields[i]+'').prop('checked', true);
+            	}
+            }, 100);
+
         }, 100);
     }
 }
