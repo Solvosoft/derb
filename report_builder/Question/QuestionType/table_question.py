6314801a0e8b5512104bb8239612298ab633db93
Mau Rodríguez Morales <maucrodriguez@gmail.com>
2016-10-25 13:54:13 -0600

    Some minor changes in the table question admin view

--- report_builder/Question/QuestionType/table_question.py
+++ report_builder/Question/QuestionType/table_question.py
@@ -3,32 +3,17 @@ Created on 20/10/2016
 
 @author: adolfo
 '''
-import json
-from django_ajax.decorators import ajax
 from django.utils.translation import ugettext as _
 
 from report_builder.Question import QuestionView
 from report_builder.Question.forms import TableQuestionForm
-from report_builder.registry import models
 
 class TableQuestionViewAdmin(QuestionView.QuestionViewAdmin):
     form_class = TableQuestionForm
     template_name = 'admin/table_question.html'
     name = 'table_question'
     minimal_representation = {
-        'human_readable_name': _('Table Question'),
+        'human_readable_name': _('Table question'),
         'help': _('Allows you to make table questions'),
         'color': '#330065'
     }
-    
-@ajax
-def get_catalog_display_fields(request):
-    if request.method == 'GET':
-        if request.is_ajax():
-            catalog_id = request.GET.get('catalog_id', False)
-            if catalog_id.isdigit():
-                catalog_id = int(catalog_id)
-                if catalog_id >= 0 and catalog_id < len(models):
-                    catalog = models[catalog_id]
-                    return catalog[3]
-    return ()
