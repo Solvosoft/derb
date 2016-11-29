893ea0f91f75b98e290e6030cb5259772d076f7e
Mau Rodríguez Morales <maucrodriguez@gmail.com>
2016-11-29 11:36:16 -0600

    Processing children questions in backend, done.

--- report_builder/templatetags/jquery_tags.py
+++ report_builder/templatetags/jquery_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.safestring import mark_safe
+from report_builder.Question.forms import NumericalSubquestionForm
 
 register = template.Library()
 
@@ -22,3 +23,14 @@ def is_jquery_widget(field, prefix=''):
         return_value = cke_editor % (str(field.id_for_label))
 
     return mark_safe(return_value)
+
+
+@register.filter(name='verbose_header')
+def verbose_header(value):
+    desc, numb = value.split('_')
+    verbose_desc = ''
+    for x in NumericalSubquestionForm.DESC_CHOICES:
+        if x[0] == desc:
+            verbose_desc = x[1]
+    new_value = verbose_desc + ' ' + numb
+    return new_value
