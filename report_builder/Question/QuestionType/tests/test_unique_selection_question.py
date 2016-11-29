cae3371b8900300764bd9352f39801ca5cc9f08a
Adolfo Hernández <fofoadolfo14@gmail.com>
2016-11-29 11:58:48 -0600

    Define setUp for admin test

--- report_builder/Question/QuestionType/tests/test_unique_selection_question.py
+++ report_builder/Question/QuestionType/tests/test_unique_selection_question.py
@@ -1,14 +1,39 @@
+import datetime
+
+from async_notifications.models import EmailTemplate
 from django.contrib.auth.models import User
 from django.urls import reverse
 
 from report_builder.Question.tests import QuestionViewAdminTest, QuestionViewRespTest
 from report_builder.Question.tests import QuestionFormTest
 
-from report_builder.models import Question, Answer, Report, ReportByProject
+from report_builder.models import Question, Answer, Report, ReportByProject, ReportType
 
 
 class UniqueSelectionQuestionViewAdminTest(QuestionViewAdminTest):
     url = 'report_builder:unique_selection_admin'
+    
+    def setUp(self):
+        User.objects.create_user(username='test', password='test')
+        report_type = ReportType.objects.create(
+            type='test_type',
+            name='test_name',
+            app_name='test.app',
+            action_ok=EmailTemplate.objects.create(code='ok_report_type_test', subject='', message=''),
+            revision_turn=EmailTemplate.objects.create(code='turn_report_type_test', subject='', message=''),
+            responsable_change=EmailTemplate.objects.create(code='change_report_type_test', subject='', message=''),
+            report_start=EmailTemplate.objects.create(code='start_report_type_test', subject='', message=''),
+            report_end=EmailTemplate.objects.create(code='end_report_type_test', subject='', message='')
+        )
+        report = Report.objects.create(type=report_type, name='Test report', opening_date=datetime.date.today())
+        Question.objects.create(
+            report=report,
+            class_to_load='unique_selection_question',
+            text='NEW UNIQUE QUESTION',
+            help='NEW UNIQUE QUESTION HELP',
+            answer_options='{\"display_fields\": [\"name\"], \"catalog\": [\"1\"]}',
+            required=Question.OPTIONAL
+        )
     
     def test_post_create_with_correct_arguments_with_login(self):
         user = User.objects.first()
